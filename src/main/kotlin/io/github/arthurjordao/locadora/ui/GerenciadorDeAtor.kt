cc0057100a68c993539b7b0e92c6b3e1e9430209
ArthurJordao <arthurbernardijordao@gmail.com>
2017-08-31 18:19:19 -0300

    Primeiros crud

---
+++ src/main/kotlin/io/github/arthurjordao/locadora/ui/GerenciadorDeAtor.kt
@@ -0,0 +1,76 @@
+package io.github.arthurjordao.locadora.ui
+
+import com.vaadin.spring.annotation.SpringUI
+import com.vaadin.ui.Component
+import io.github.arthurjordao.locadora.model.Ator
+import io.github.arthurjordao.locadora.repository.AtorRepository
+import org.vaadin.viritin.fields.MTextField
+import org.vaadin.viritin.form.AbstractForm
+import org.vaadin.viritin.grid.MGrid
+import org.vaadin.viritin.layouts.MVerticalLayout
+
+@SpringUI(path = "/ator")
+class GerenciadorDeAtor(val repository: AtorRepository) : TabelaForm<Ator>() {
+
+    override val tabela: MGrid<Ator> = MGrid(Ator::class.java)
+
+    override val formulario: AbstractForm<Ator> = FormularioDeAtor(this)
+
+    override fun atualizaTabela() {
+        tabela.setItems(repository.findAll())
+    }
+
+    override fun salva(entity: Ator) {
+        repository.save(entity)
+        atualizaTabela()
+    }
+
+    override fun getEntity(id: Long): Ator {
+        return if (repository.exists(id))
+            repository.findOne(id)
+        else
+            Ator()
+    }
+
+    override fun remove(entity: Ator) {
+        if (repository.exists(entity.id))
+            repository.delete(entity)
+        atualizaTabela()
+    }
+
+    override fun configuraTabela() {
+        super.configuraTabela()
+        tabela.removeColumn("filmes")
+        tabela.setColumnOrder("id", "nome")
+    }
+
+}
+
+class FormularioDeAtor(val tabela: Tabela<Ator>) : AbstractForm<Ator>(Ator::class.java) {
+
+    val nome = MTextField("Nome")
+
+    init {
+        configuraBotoes()
+        entity = Ator()
+    }
+
+    private fun configuraBotoes() {
+        setSavedHandler {
+            tabela.salva(it)
+            entity = Ator()
+        }
+        setResetHandler {
+            entity = tabela.getEntity(it.id)
+        }
+        setDeleteHandler {
+            tabela.remove(it)
+            entity = Ator()
+        }
+    }
+
+    override fun createContent(): Component {
+        return MVerticalLayout(nome, toolbar)
+    }
+
+}
---
+++ src/main/kotlin/io/github/arthurjordao/locadora/ui/GerenciadorDeClientesUI.kt
@@ -0,0 +1,75 @@
+package io.github.arthurjordao.locadora.ui
+
+import com.vaadin.spring.annotation.SpringUI
+import com.vaadin.ui.Component
+import io.github.arthurjordao.locadora.model.Cliente
+import io.github.arthurjordao.locadora.repository.ClienteRepository
+import org.vaadin.viritin.fields.MTextField
+import org.vaadin.viritin.form.AbstractForm
+import org.vaadin.viritin.grid.MGrid
+import org.vaadin.viritin.layouts.MVerticalLayout
+
+@SpringUI(path = "/clientes")
+class GerenciadorDeClientesUI(val repository: ClienteRepository) : TabelaForm<Cliente>() {
+    override val tabela: MGrid<Cliente> = MGrid<Cliente>(Cliente::class.java)
+    override val formulario: AbstractForm<Cliente> = FormularioCliente(this)
+
+    override fun salva(entity: Cliente) {
+        repository.save(entity)
+        atualizaTabela()
+    }
+
+    override fun remove(entity: Cliente) {
+        if (repository.exists(entity.id))
+            repository.delete(entity)
+        atualizaTabela()
+    }
+
+    override fun atualizaTabela() {
+        tabela.setItems(repository.findAll())
+    }
+
+    override fun getEntity(id: Long): Cliente {
+        return if (repository.exists(id))
+            return repository.findOne(id)
+        else
+            Cliente()
+    }
+
+    override fun configuraTabela() {
+        super.configuraTabela()
+        tabela.setColumnOrder("id", "nome", "telefone", "cpf", "endereco")
+    }
+
+}
+
+class FormularioCliente(val tabela: Tabela<Cliente>) : AbstractForm<Cliente>(Cliente::class.java) {
+    val nome = MTextField("Nome")
+    val telefone = MTextField("Telefone")
+    val cpf = MTextField("Cpf")
+    val endereco = MTextField("Endereço")
+
+    init {
+        configuraBotoes()
+        entity = Cliente()
+    }
+
+    private fun configuraBotoes() {
+        setSavedHandler {
+            tabela.salva(it)
+            entity = Cliente()
+        }
+        setResetHandler {
+            entity = tabela.getEntity(it.id)
+        }
+        setDeleteHandler {
+            tabela.remove(it)
+            entity = Cliente()
+        }
+    }
+
+    override fun createContent(): Component {
+        return MVerticalLayout(nome, telefone, cpf, endereco, toolbar)
+    }
+
+}
